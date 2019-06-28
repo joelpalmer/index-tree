8612430b6d32f589d00de16a0ef932c831e0e8a4
Joel Palmer <joelpalmer@gmail.com>
2019-06-28 14:21:51 -0500

    impl<T> Arena<T>

--- src/lib.rs
+++ src/lib.rs
@@ -92,3 +92,129 @@ pub struct Node<T> {
     /// The actual data which will be stored within the tree
     pub data: T,
 }
+
+impl<T> fmt::Display for Node<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(parent) = self.parent {
+            write!(f, "parent: {}; ", parent)?;
+        } else {
+            write!(f, "no parent; ")?;
+        }
+        if let Some(previous_sibling) = self.previous_sibling {
+            write!(f, "previous sibling: {}; ", previous_sibling)?;
+        } else {
+            write!(f, "no previous sibling; ")?;
+        }
+        if let Some(next_sibling) = self.next_sibling {
+            write!(f, "next sibling: {}; ", next_sibling)?;
+        } else {
+            write!(f, "no next sibling; ")?;
+        }
+        if let Some(first_child) = self.first_child {
+            write!(f, "first child: {}; ", first_child)?;
+        } else {
+            write!(f, "no first child; ")?;
+        }
+        if let Some(last_child) = self.last_child {
+            write!(f, "last child: {}; ", last_child)?;
+        } else {
+            write!(f, "no last child; ")?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "deser", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "derive-eq", derive(Eq))]
+/// An `Arena` structure containing certain Nodes
+pub struct Arena<T> {
+    nodes: Vec<Node<T>>,
+}
+
+impl<T> Arena<T> {
+    /// Create a new empty `Arena`
+    pub fn new() -> Arena<T> {
+        Arena { nodes: Vec::new() }
+    }
+
+    /// Creates a new node from its associated data.
+    ///
+    /// #Panics
+    ///
+    /// Panics if the arena already has `usize::max_value()` nodes.
+    pub fn new_node(&mut self, data: T) -> NodeId {
+        let next_index1 = NonZeroUsize::new(self.nodes.len().wrapping_add(1))
+            .expect("Too many nodes in the arena");
+        self.nodes.push(Node {
+            parent: None,
+            first_child: None,
+            last_child: None,
+            previous_sibling: None,
+            next_sibling: None,
+            removed: false,
+            data,
+        });
+        NodeId::from_non_zero_usize(next_index1)
+    }
+
+    /// Count nodes in the arena
+    pub fn count(&self) -> usize {
+        self.nodes.len()
+    }
+
+    /// Return true if arena has no nodes, false otherwise
+    pub fn is_empty(&self) -> bool {
+        self.count() == 0
+    }
+
+    /// Get a reference to the node with the given id if in the arena,
+    /// None otherwise.
+    pub fn get(&self, id: NodeId) -> Option<&Node<T>> {
+        self.nodes.get(id.index0())
+    }
+
+    /// Get a mutable reference to the node with the given id if in the arena,
+    /// None otherwise
+    pub fn get_mut(&mut self, id: NodeId) -> Option<&mut Node<T>> {
+        self.nodes.get_mut(id.index0())
+    }
+
+    /// Iterate over all the nodes in the arena in storage-order.
+    ///
+    /// Note that this iterator also contains removed elements which can be
+    /// tested with the `is_removed()` method on the node.
+    pub fn iter(&self) -> std::slice::Iter<Node<T>> {
+        self.nodes.iter()
+    }
+}
+
+impl NodeId {
+    /// Returns zero-based index.
+    fn index0(self) -> usize {
+        // Safe because `self.index1 >= 1` is guaranteed by
+        // `NonZeroUsize` type
+        self.index1.get() - 1
+    }
+
+    /// Create a `NodeId` used for attempting to get `Node`s references from an
+    /// `Arena`.
+    ///
+    /// Note that a zero-based index should be given.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the value is `usize::max_value()`.
+    pub fn new(index0: usize) -> Self {
+        let index1 = NonZeroUsize::new(index0.wrapping_add(1))
+            .expect("Attempt to create `NodeId` from `usize::max_value()`");
+        NodeId { index1 }
+    }
+
+    /// Creates a new `NodeId` from the given one-based index.
+    pub fn from_non_zero_usize(index1: NonZeroUsize) -> Self {
+        NodeId { index1 }
+    }
+
+    // MORE...
+}
